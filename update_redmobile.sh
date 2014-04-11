9c8f8421294e845f214eb8cf06af86fff6f2be98
byeonggee.seo <byeonggee.seo@redduck.com>
2014-04-11 18:29:38 +0900

    ???

---
+++ update_redmobile.sh
@@ -0,0 +1,6 @@
+rm -rf redmobile/tools
+cp -r tools redmobile/
+rm -rf redmobile/frameworks/js-bindings/cocos2d-x
+cp -r frameworks/js-bindings/cocos2d-x redmobile/frameworks/js-bindings/
+rm -rf redmobile/frameworks/js-bindings/bindings
+cp -r frameworks/js-bindings/bindings redmobile/frameworks/js-bindings/
